e73af8c12f101248c9f23f3a3ec66d7a60eb1e69
Lorraine Thomas <116093083+lorraineathomas@users.noreply.github.com>
2025-07-04 13:26:29 -0400

    component return statement

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,11 @@
 export default function Results(props) {
   if (props.results) {
-    return <div className="Results">Hello from results</div>;
+    return (
+      <div className="Results">
+        <h3>{props.results.word}</h3>; return{" "}
+        <div className="Results">Hello from results</div>;
+      </div>
+    );
   } else {
     return null;
   }
