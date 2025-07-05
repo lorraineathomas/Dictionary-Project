dee0d3704bba8e379e1d4f635d965e2a481a1e92
Lorraine Thomas <116093083+lorraineathomas@users.noreply.github.com>
2025-07-05 11:40:23 -0400

    displayed word, definition, part of speech, phonetics, and phonetics

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,20 @@
-import react, { useState } from "react";
-import Results from "./Results";
+import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setkeyword] = useState("");
-  let [results, setResults] = useState("");
 
   function handleResponse(response) {
+    console.log(response.data[0].meanings[0].definitions[0].definition);
     console.log(response.data[0]);
-    setResults = response.data[0];
-    //let def = document.querySelector(".definition");
-    //let defined = response.data[0].meanings[0].definitions[0].definition;
-    //def.innerHTML = defined;
+    let resultCall = document.querySelector(".results");
+    resultCall.innerHTML = `<strong>${response.data[0].word}</strong><br/>${response.data[0].meanings[0].partOfSpeech}<br/>${response.data[0].phonetic}<br/>${response.data[0].meanings[0].definitions[0].definition}`;
   }
 
   function search(event) {
     event.preventDefault();
+    // Documentation : https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     //let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key={73a673doafc73bccb4ff04ct60632486}`;
     axios.get(apiUrl).then(handleResponse);
@@ -32,8 +30,8 @@ export default function Dictionary() {
       <form onSubmit={search} className="text-center">
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
         <input type="submit" value="Search" />
-        <Results results={results} />
       </form>
+      <div className="results"></div>
     </div>
   );
 }
